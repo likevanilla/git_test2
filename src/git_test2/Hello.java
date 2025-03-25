0f77bb6e746414ef628dc213c4678d837e6fc81a
likevanilla <wjdgur27507@gmail.com>
2025-03-25 18:14:35 +0900

    Hello 클래스 수정

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -4,6 +4,14 @@ public class Hello {
 
 	String message = "안녕하세요";
 
+	public Hello() {
+		this("안녕하세요");
+	}
+
+	public Hello(String message) {
+		this.message = message;
+	}
+
 	public void say() {
 		System.out.println(message);
 	}
